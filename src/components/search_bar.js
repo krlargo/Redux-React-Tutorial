59c6bca32e858e6a23b20f898fdb0d0598db3d20
krlargo <krlargo@ucdavis.edu>
2017-12-20 02:44:10 -0800

    Convert to class-based component

---
+++ src/components/search_bar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Doesn't need DOM because this will be anchored onto something else
+//import ReactDOM from 'react-dom';
+
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar; // The outward end of an import statement
