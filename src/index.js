a8dfeb6860e1a58268c1a63990196701218b1251
krlargo <krlargo@ucdavis.edu>
2017-12-20 15:40:55 -0800

    Test YouTube search API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 // File directory needed for user-created files
 import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyD04icmkiPEJ3jZumEPfwzAZw-GJ9UdODc";
+
+YTSearch({key: API_KEY, term: 'Yeezy'}, function(data) {
+  console.log(data);
+});
 
 // Define component blueprint
 const App = () => {
