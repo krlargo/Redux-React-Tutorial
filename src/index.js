5c9c3c0c263a4d258deddbc162280b61bf0089ff
krlargo <krlargo@ucdavis.edu>
2017-12-20 02:44:41 -0800

    Insert SearchBar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// File directory needed for user-created files
+import SearchBar from './components/search_bar';
+
 const API_KEY = "AIzaSyD04icmkiPEJ3jZumEPfwzAZw-GJ9UdODc";
 
 // Define component blueprint
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Use <App /> to instantiate component
