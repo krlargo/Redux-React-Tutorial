5f5d828aba5ba5366b71c448bf28e37d1601fde6
krlargo <krlargo@ucdavis.edu>
2017-12-20 15:57:43 -0800

    Add VideoList component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
-// File directory needed for user-created files
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = "AIzaSyD04icmkiPEJ3jZumEPfwzAZw-GJ9UdODc";
 
-YTSearch({key: API_KEY, term: 'Yeezy'}, function(data) {
-  console.log(data);
-});
-
 // Define component blueprint
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // Load some video JSONs
+    YTSearch({key: API_KEY, term: 'Yeezy'}, (videos) => {
+      this.setState({ videos }); // same as videos: videos
+    });
+  }
+
   render() {
+    // Pass 'videos' to VidoeList's props
     return (
       <div>
         <SearchBar />
-        </div>
-      );
-    }
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Use <App /> to instantiate component
