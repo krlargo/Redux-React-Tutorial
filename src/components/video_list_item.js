06f91561e5bbf88ed29ac9da067a367ba82785f4
krlargo <krlargo@ucdavis.edu>
2017-12-20 16:55:39 -0800

    Parse title and thumbnail url

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+// Accepting {video} parameter is the same as
+// const video = props.video;
+const VideoListItem = ({video}) => {
+
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const title = video.snippet.title;
+
   return(
-    <li>Video</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{title}</div>
+        </div>
+      </div>
+    </li>
   )
 }
 
